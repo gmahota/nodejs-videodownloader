0974fa41e0fddf17d2ae517517cb274aec6355c0
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-02-15 21:58:27 +0200

    add url-video

--- web/script.js
+++ web/script.js
@@ -1,7 +1,7 @@
 let Btn = document.getElementById('btn');
 let URLinput = document.querySelector('.URL-input');
 let select = document.querySelector('.opt');
-let serverURL = 'http://localhost:4000';
+let serverURL = 'https://vmzdownload.herokuapp.com';
 
 Btn.addEventListener('click', () => {
 	if (!URLinput.value) {
