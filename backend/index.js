fafe1f4ec061dcd559a9f341a5accb1b4ce50a4a
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-06-26 11:50:35 +0200

    update video download version

--- backend/index.js
+++ backend/index.js
@@ -12,50 +12,51 @@ app.listen(PORT, () => {
 });
 
 app.get("/youtube/downloadmp3", async (req, res, next) => {
-    try {
-      var url = req.query.url;
-      if (!ytdl.validateURL(url)) {
-        return res.sendStatus(400);
-      }
-      let title = "audio";
-  
-      // await ytdl.getBasicInfo(url, {
-      // 	format: 'mp4'
-      // }, (err, info) => {
-      // 	if (err) throw err;
-      // 	title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-      // });
-  
-      res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
-      ytdl(url, {
-        format: "mp3",
-        filter: "audioonly",
-      }).pipe(res);
-    } catch (err) {
-      console.error(err);
+  try {
+    var url = req.query.url;
+    if (!ytdl.validateURL(url)) {
+      return res.sendStatus(400);
     }
-  });
+    let title = "audio";
+
+    // await ytdl.getBasicInfo(url, {
+    // 	format: 'mp4'
+    // }, (err, info) => {
+    // 	if (err) throw err;
+    // 	title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+    // });
+
+    res.header("Content-Disposition", `attachment; filename="${title}.mp3"`);
+    ytdl(url, {
+      format: "mp3",
+      filter: "audioonly",
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 app.get("/youtube/downloadmp4", async (req, res, next) => {
-    try {
-      let url = req.query.url;
-      if (!ytdl.validateURL(url)) {
-        return res.sendStatus(400);
-      }
-      let title = "video";
-  
-      // await ytdl.getBasicInfo(url, {
-      // 	format: 'mp4'
-      // }, (err, info) => {
-  
-      // 	title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-      // });
-      res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
-      await ytdl(url, {
-        format: "mp4",
-      }).pipe(res);
-    } catch (err) {
-      console.error(err);
+  try {
+    let url = req.query.url;
+    if (!ytdl.validateURL(url)) {
+      return res.sendStatus(400);
     }
-  });
-  
+    let title = "video";
+
+    // await ytdl.getBasicInfo(url, {
+    // 	format: 'mp4'
+    // }, (err, info) => {
+
+    // 	title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+    // });
+
+    res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
+
+    await ytdl(url, {
+      format: "mp4",
+    }).pipe(res);
+  } catch (err) {
+    console.error(err);
+  }
+});
